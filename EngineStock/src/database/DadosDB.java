b7089fb81ae3b7c3a6e97a68eb812dea76a08690
DeividiMartin <deividi_97@hotmail.com>
2023-09-30 01:17:32 -0300

    StockEngine 1.0

--- EngineStock/src/database/DadosDB.java
+++ EngineStock/src/database/DadosDB.java
@@ -1,11 +1,11 @@
 package database;
 
 public class DadosDB {
-    String url = "mysql://root:8P43FIuJNLiY1tzSImf4@containers-us-west-103.railway.app:6263/railway";
-    String usuario = "root";
-    String senha = "8P43FIuJNLiY1tzSImf4";
+    String url = "";
+    String usuario = "";
+    String senha = "";
 	
-    String connectionString = "jdbc:mysql://containers-us-west-103.railway.app:6263/railway?user=root&password=8P43FIuJNLiY1tzSImf4";
+    String connectionString = "";
 
 
 	 
